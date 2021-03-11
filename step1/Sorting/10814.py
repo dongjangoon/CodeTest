6c707a1b3201381489a345b974dfd88faa4aae10
numnum97 <numnum97@naver.com>
2021-03-11 22:32:03 +0900

    3/11

--- step1/Sorting/10814.py
+++ step1/Sorting/10814.py
@@ -1,7 +1,7 @@
 n = int(input())
 
 def quickSort(A, p, r):
-    
+
     if p < r:
         q = partition(A, p, r)
         quickSort(A, p, q-1)
@@ -11,21 +11,22 @@ def partition(A, p, r):
 
     x, i = A[r], p-1
     for j in range(p, r):
-        if A[j][1] < x[1]:
+        if A[j][0] < x[0]:
             i += 1
             A[i], A[j] = A[j], A[i]
-        elif A[j][1] == x[1]:
-            if A[j][0] < x[0]:
+        elif A[j][0] == x[0]:
+            if A[j][2] < x[2]:
                 i += 1
                 A[i], A[j] = A[j], A[i]
     
     A[r], A[i+1] = A[i+1], A[r]
     return i+1
 
-a= []
+a = []
 for i in range(n):
-    x, y = map(int, input().split())
-    a.append((x,y))
+    b = input().split()
+    age, name = b[0], b[1]
+    a.append((int(age), name, i))
 
 quickSort(a, 0, n-1)
 
